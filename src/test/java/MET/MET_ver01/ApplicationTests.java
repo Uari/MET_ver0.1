8e6b2eee4f655a37c90f1974f46079ae06d6acd1
alstn4676 <59790492+alstn4676@users.noreply.github.com>
2022-03-17 15:16:11 +0900

    Initial commit

---
+++ src/test/java/MET/MET_ver01/ApplicationTests.java
@@ -0,0 +1,13 @@
+package MET.MET_ver01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
